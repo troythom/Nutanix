2c1541dd6e01d1eaed8b6f61e91896012f96711b
Troy Thompson <77289695+troythom@users.noreply.github.com>
2021-12-23 10:28:20 -0500

    Update GetStoragUtilization - Working on Export to CSV

--- Powershell Cmdlets v2.0/GetStorageUtilization.ps1
+++ Powershell Cmdlets v2.0/GetStorageUtilization.ps1
@@ -1,4 +1,4 @@
-#Get Stats on a Storage Container
+#Retrieve Container Utilization Statistics (Derive "Allocated" Space)
 #Utilize Nutanix Cmdlets v2.0 and Rest API v2.0
 
 #Created by Troy Thompson
@@ -18,7 +18,7 @@ $headers.Add("content-type", "application/json")
 $Credential = Get-Credential
 
 #Connect to prism Central
-Connect-PrismCentral -Server $PC -AcceptInvalidSSLCerts -Credential $credential -ForcedConnection -SessionTimeoutSeconds 3600
+#Connect-PrismCentral -Server $PC -AcceptInvalidSSLCerts -Credential $credential -ForcedConnection -SessionTimeoutSeconds 3600
 
 #Get List of Clusters
 $URI = "https://" + $PC + ":9440/api/nutanix/v3/clusters/list"
@@ -26,5 +26,36 @@ $Clusters = Invoke-RestMethod -Uri $URI -SkipCertificateCheck -Authentication Ba
 
 $ClusterVips = Write-Output -InputObject $Clusters.entities.spec.resources.network.external_ip
 
-$ClusterVips
+Clear-Host
+Write-Host "List of Cluster Virtual IP Addresses"
+Write-Output -InputObject $ClusterVips
+$TargetCluster = Read-Host -Prompt "Enter the Cluster vIP to Analyze"
+
+#Get list of Storage Containers
+$PEURI = "https://" + $TargetCluster + ":9440/PrismGateway/services/rest/v2.0/storage_containers"
+$ContanierResponse = Invoke-RestMethod -Uri $PEURI -SkipCertificateCheck -Authentication Basic -Credential $Credential -Method GET 
+
+
+$Metrics = foreach ($temp in $ContanierResponse) {
+    [pscustomobject] @{
+        Container_Name = $temp.entities.name -join ', '
+        Max_Capacity = $temp.entities.max_capacity -join ', '
+        Logical_Used_Bytes = $temp.entities.usage_stats."storage.user_unreserved_own_usage_bytes" -join ', '
+        Total_Reduction_Bytes = $temp.entities.usage_stats."data_reduction.overall.user_saved_bytes" -join ', '
+    } | Select-Object -Property Container_Name, Max_Capacity, Logical_Used_Bytes, Total_Reduction_Bytes 
+}
+
+# Working in Progress export to CSV or TXT File
+
+$fileToCheck = "c:\TEMP\metrics.txt"
+        if (Test-Path $fileToCheck -PathType leaf)
+        {
+            Remove-Item $fileToCheck
+        }
+
+$Metrics | Out-String | Set-Content c:\TEMP\metrics.txt
+
+
+#Invoke-Item c:\TEMP\metrics.csv
+
 
