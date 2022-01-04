1afc32dca4e4126dcd82b466184325ec88e7f4d8
Troy Thompson <77289695+troythom@users.noreply.github.com>
2022-01-04 13:30:05 -0500

    Update

--- Powershell Cmdlets v2.0/ConnectToPC.ps1
+++ Powershell Cmdlets v2.0/ConnectToPC.ps1
@@ -3,14 +3,10 @@
 
 #Prompt for Environment Inputs
 $PC = Read-Host -Prompt 'Input FQDN or IP Address of the target Prism Central Instance'
-$PCUser = Read-Host -Prompt 'Input PC User ID'
-$Password = Read-Host -Prompt 'Input PC User Password'
+
 
 #Set the Credentials
-$SecurePW = ConvertTo-SecureString $Password -AsPlainText -Force
-$Credential = New-Object System.Management.Automation.PSCredential ($PCUser, $SecurePW);
+$Credential = Get-Credential
 
-#Connect
-Connect-PrismCentral -Server $PC -AcceptInvalidSSLCerts -Credential $Credential -ForcedConnection -SessionTimeoutSeconds 3600
-
-
+#Connect to prism Central
+Connect-PrismCentral -Server $PC -AcceptInvalidSSLCerts -Credential $credential -ForcedConnection -SessionTimeoutSeconds 3600
