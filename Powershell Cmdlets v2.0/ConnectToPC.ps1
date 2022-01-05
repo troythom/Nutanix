670b626a3293ea1151ab7ea3a45dc7fca3eecc09
Troy Thompson <77289695+troythom@users.noreply.github.com>
2022-01-05 14:42:06 -0500

    Update

--- Powershell Cmdlets v2.0/ConnectToPC.ps1
+++ Powershell Cmdlets v2.0/ConnectToPC.ps1
@@ -2,11 +2,15 @@
 #Utilize Nutanix Cmdlets v2.0
 
 #Prompt for Environment Inputs
-$PC = Read-Host -Prompt 'Input FQDN or IP Address of the target Prism Central Instance'
+#$PC = Read-Host -Prompt 'Input FQDN or IP Address of the target Prism Central Instance'
 
+#Setup Mandatory Parameters
+param (
+    [Parameter(Mandatory=$true)][string]$PCvIP
+)
 
 #Set the Credentials
 $Credential = Get-Credential
 
 #Connect to prism Central
-Connect-PrismCentral -Server $PC -AcceptInvalidSSLCerts -Credential $credential -ForcedConnection -SessionTimeoutSeconds 3600
+Connect-PrismCentral -Server $PCvIP -AcceptInvalidSSLCerts -Credential $credential -ForcedConnection -SessionTimeoutSeconds 3600
