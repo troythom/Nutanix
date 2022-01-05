92580a1121a369a243c6b946ac7857b8dd0faf7a
Troy Thompson <77289695+troythom@users.noreply.github.com>
2022-01-05 14:36:41 -0500

    Update with proper .csv export function

--- Powershell Cmdlets v2.0/GetStorageUtilization.ps1
+++ Powershell Cmdlets v2.0/GetStorageUtilization.ps1
@@ -4,24 +4,24 @@
 #Created by Troy Thompson
 #troy.thompson@nutanix.com
 #Created - 07-December-2021
+#Updated - 05-January-2022
 
-Clear-Host
-
-#Prompt for Environment Info
-$PC = Read-Host -Prompt "Enter the FQDN or IP Address of the Prism Central Instance"
+#Setup Mandatory Parameters
+param (
+    [Parameter(Mandatory=$true)][string]$PCvIP
+    #[Parameter(Mandatory=$true)][string]$TargetCluster
+)
 
 #Setup Basic Header Info
 $headers=@{}
 $headers.Add("content-type", "application/json")
 
 #Establish Prism Central Credentials
+Clear-Host
 $Credential = Get-Credential
 
-#Connect to prism Central
-#Connect-PrismCentral -Server $PC -AcceptInvalidSSLCerts -Credential $credential -ForcedConnection -SessionTimeoutSeconds 3600
-
 #Get List of Clusters
-$URI = "https://" + $PC + ":9440/api/nutanix/v3/clusters/list"
+$URI = "https://" + $PCvIP + ":9440/api/nutanix/v3/clusters/list"
 $Clusters = Invoke-RestMethod -Uri $URI -SkipCertificateCheck -Authentication Basic -Credential $Credential -Method POST -Headers $headers -Body '{"kind":"cluster"}'
 
 $ClusterVips = Write-Output -InputObject $Clusters.entities.spec.resources.network.external_ip
@@ -29,33 +29,31 @@ $ClusterVips = Write-Output -InputObject $Clusters.entities.spec.resources.netwo
 Clear-Host
 Write-Host "List of Cluster Virtual IP Addresses"
 Write-Output -InputObject $ClusterVips
-$TargetCluster = Read-Host -Prompt "Enter the Cluster vIP to Analyze"
+$TargetCluster = Read-Host -Prompt "Enter the Prism Element Cluster vIP to Analyze"
 
 #Get list of Storage Containers
 $PEURI = "https://" + $TargetCluster + ":9440/PrismGateway/services/rest/v2.0/storage_containers"
 $ContanierResponse = Invoke-RestMethod -Uri $PEURI -SkipCertificateCheck -Authentication Basic -Credential $Credential -Method GET 
 
+#Place the results of each desired value into an array
+[array]$ContName = $ContanierResponse.entities.name
+[array]$MaxCap = $ContanierResponse.entities.max_capacity
+[array]$LogicalUsed = $ContanierResponse.entities.usage_stats."storage.user_unreserved_own_usage_bytes"
+[array]$TotalReduce = $ContanierResponse.entities.usage_stats."data_reduction.overall.user_saved_bytes"
 
-$Metrics = foreach ($temp in $ContanierResponse) {
-    [pscustomobject] @{
-        Container_Name = $temp.entities.name -join ', '
-        Max_Capacity = $temp.entities.max_capacity -join ', '
-        Logical_Used_Bytes = $temp.entities.usage_stats."storage.user_unreserved_own_usage_bytes" -join ', '
-        Total_Reduction_Bytes = $temp.entities.usage_stats."data_reduction.overall.user_saved_bytes" -join ', '
-    } | Select-Object -Property Container_Name, Max_Capacity, Logical_Used_Bytes, Total_Reduction_Bytes 
-}
-
-# Working in Progress export to CSV or TXT File
-
-$fileToCheck = "c:\TEMP\metrics.csv"
+#Cleanup any old files
+$fileToCheck = "c:\TEMP\$targetCluster.csv"
         if (Test-Path $fileToCheck -PathType leaf)
         {
             Remove-Item $fileToCheck
         }
 
-Export-Csv -InputObject $Metrics -Path c:\TEMP\metrics.csv  
-
-
-#Invoke-Item c:\TEMP\metrics.csv
-
-
+#Export the contents of each array into a custom object and then export to CSV with individual rows
+0..($ContName.Count-1) | ForEach-Object {
+    [pscustomobject]  @{
+        "Container_Name" = $ContName[$_]
+        "Maximum_Capacity" = $MaxCap[$_]
+        "Logical_Space_Used" = $LogicalUsed[$_]
+        "Total_Reduction" = $TotalReduce[$_]
+    }
+} | Export-Csv -Path c:\TEMP\$TargetCluster.csv -NoTypeInformation
